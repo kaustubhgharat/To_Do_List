df3552fde621567d6be5a6a3c4ce4208c71e57e1
kaustubhgharat <gharatkaustubh36@gmail.com>
2026-04-08 20:12:37 +0530

    ...

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,8 +1,7 @@
 import dotenv from "dotenv";
 
-// Load .env file
 dotenv.config();
 
-export const PORT = process.env.PORT || 5000;
 export const MONGO_URI = process.env.MONGO_URI;
+export const PORT = process.env.PORT || 5000;
 export const JWT_SECRET = process.env.JWT_SECRET || "default_secret_change_this";
